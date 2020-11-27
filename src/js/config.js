2f2af62ffeb14a62d6904bc8f93f36ad63cdee9b
huyhung <huyhung2752019@gmail.com>
2020-11-27 21:08:27 +0700

    Init commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIME_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'eece0549-9363-482b-83d3-eca332a96970';
+export const MODAL_CLOSE_SEC = 2.5;
